2fb373f99cc5b3a36f30f4bae0e2ea72de064e93
mohitchowdaryy <99123486+mohitchowdaryy@users.noreply.github.com>
2022-02-06 21:31:11 +0530

    Update encoding.c

--- encoding.c
+++ encoding.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    char str[5];
+    printf("Enter the String to be Encode :\n");
+    for(int i=0;i<5;i++)
+    {
+    scanf("%c",&str[i]);
+    }
+   
+    int i = 0;
+ 
+   while (str[i] != '\0') {
+      str[i] = str[i] - 30;  // Subtract 30 From Charcter
+      i++;
+   }
+  
+   printf("Encoded string is %s",str);
+   
+}
 
